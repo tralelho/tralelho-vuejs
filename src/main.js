0e525687b8c998782423a58783baaae5278727f0
edieval <emmanuel.dieval@adeo.com>
2021-10-04 22:22:02 +0200

    chore: install bulma & font awesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./assets/main.sass";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount("#app");
+library.add(faUserSecret);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
