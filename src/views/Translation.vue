4432dfd249e5bc522c06f0a47eac3ab378db6534
edieval <emmanuel.dieval@adeo.com>
2021-11-15 22:32:35 +0100

    feat: add side menu to translation page

--- src/views/Translation.vue
+++ src/views/Translation.vue
@@ -6,24 +6,46 @@ const { locale } = useI18n({ useScope: "global" });
 
 <template>
   <div class="section">
-    <h1>Translation Page locale: {{ locale }}</h1>
+    <h1 class="title is-1">Translation Page locale: {{ locale }}</h1>
 
-    <div class="container mb-6" v-for="section of pageConfig" :key="section.title">
-      <h2 class="title is-2 has-text-left" v-t="{ path: section.title }"></h2>
+    <div className="columns">
+      <div className="column is-1 has-text-left">
+        <aside class="menu">
+          <ul class="menu-list">
+            <li v-for="section of pageConfig" :key="section.title">
+              <a :href="'#' + section.title" v-t="{ path: section.title }"></a>
+            </li>
+          </ul>
+        </aside>
+      </div>
 
-      <div
-        v-for="phrase of section.list"
-        :key="phrase"
-        class="box columns m-2 py-2 is-clickable change-background-on-hover"
-      >
-        <div class="column is-half py-1">
-          <p v-t="{ path: phrase }" class="bd-notification is-primary"></p>
-        </div>
-        <div class="column is-half py-1">
-          <p
-            v-t="{ path: phrase, locale: $route.query.lang }"
-            class="bd-notification is-primary"
-          ></p>
+      <div className="column">
+        <div
+          class="container mb-6"
+          v-for="section of pageConfig"
+          :key="section.title"
+        >
+          <h2
+            :id="section.title"
+            class="title is-2 has-text-left"
+            v-t="{ path: section.title }"
+          ></h2>
+
+          <div
+            v-for="phrase of section.list"
+            :key="phrase"
+            class="box columns m-2 py-2 is-clickable change-background-on-hover"
+          >
+            <div class="column is-half py-1">
+              <p v-t="{ path: phrase }" class="bd-notification is-primary"></p>
+            </div>
+            <div class="column is-half py-1">
+              <p
+                v-t="{ path: phrase, locale: $route.query.lang }"
+                class="bd-notification is-primary"
+              ></p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
