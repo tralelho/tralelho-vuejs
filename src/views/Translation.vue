e1967a8384cdb7a9c32fe044bcf8ce3e09c6475c
edieval <emmanuel.dieval@adeo.com>
2021-11-15 21:53:21 +0100

    refactor: configure the translation page

--- src/views/Translation.vue
+++ src/views/Translation.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
+import pageConfig from "./Translation.config.json";
 const { locale } = useI18n({ useScope: "global" });
 </script>
 
@@ -7,19 +8,23 @@ const { locale } = useI18n({ useScope: "global" });
   <div class="section">
     <h1>Translation Page locale: {{ locale }}</h1>
 
-    <div
-      v-for="index of 857"
-      :key="index"
-      class="box columns m-2 is-clickable change-background-on-hover"
-    >
-      <div class="column is-half">
-        <p v-t="{ path: index }" class="bd-notification is-primary"></p>
-      </div>
-      <div class="column is-half">
-        <p
-          v-t="{ path: index, locale: $route.query.lang }"
-          class="bd-notification is-primary"
-        ></p>
+    <div class="container mb-6" v-for="section of pageConfig" :key="section.title">
+      <h2 class="title is-2 has-text-left" v-t="{ path: section.title }"></h2>
+
+      <div
+        v-for="phrase of section.list"
+        :key="phrase"
+        class="box columns m-2 is-clickable change-background-on-hover"
+      >
+        <div class="column is-half">
+          <p v-t="{ path: phrase }" class="bd-notification is-primary"></p>
+        </div>
+        <div class="column is-half">
+          <p
+            v-t="{ path: phrase, locale: $route.query.lang }"
+            class="bd-notification is-primary"
+          ></p>
+        </div>
       </div>
     </div>
   </div>
