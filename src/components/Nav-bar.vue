d3bfc614f269658869deaeb6bfc362ceeb0336bc
edieval <emmanuel.dieval@adeo.com>
2021-11-29 22:00:23 +0100

    feat: animate navbar

--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -56,43 +56,43 @@
             ></router-link>
 
             <router-link
-                :to="{
+              :to="{
                 name: 'samu',
                 params: { locale },
                 query: { lang: 'deu' },
               }"
-                class="navbar-item"
-                v-t="{ path: 681, locale: 'fra' }"
+              class="navbar-item"
+              v-t="{ path: 681, locale: 'fra' }"
             ></router-link>
 
             <router-link
-                :to="{
+              :to="{
                 name: 'samu',
                 params: { locale },
                 query: { lang: 'spa' },
               }"
-                class="navbar-item"
-                v-t="{ path: 744, locale: 'fra' }"
+              class="navbar-item"
+              v-t="{ path: 744, locale: 'fra' }"
             ></router-link>
 
             <router-link
-                :to="{
+              :to="{
                 name: 'samu',
                 params: { locale },
                 query: { lang: 'por' },
               }"
-                class="navbar-item"
-                v-t="{ path: 730, locale: 'fra' }"
+              class="navbar-item"
+              v-t="{ path: 730, locale: 'fra' }"
             ></router-link>
 
             <router-link
-                :to="{
+              :to="{
                 name: 'samu',
                 params: { locale },
                 query: { lang: 'ita' },
               }"
-                class="navbar-item"
-                v-t="{ path: 700, locale: 'fra' }"
+              class="navbar-item"
+              v-t="{ path: 700, locale: 'fra' }"
             ></router-link>
           </div>
         </div>
@@ -123,6 +123,31 @@ export default {
     return {
       locale,
     };
+  },
+  setup() {
+    document.addEventListener("DOMContentLoaded", () => {
+      // Get all "navbar-burger" elements
+      const $navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll(".navbar-burger"),
+        0
+      );
+
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach((el) => {
+          el.addEventListener("click", () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle("is-active");
+            $target.classList.toggle("is-active");
+          });
+        });
+      }
+    });
   },
 };
 </script>
