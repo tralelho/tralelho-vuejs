11643116151d0bf6a10eb1617fb15c2e9c442f09
Tralelho-Marion <84783201+tralelho-github@users.noreply.github.com>
2021-12-16 11:25:11 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,49 @@
 
 <template>
   <div class="section">
-    <h1 class="title">Soutenir TraLELHo</h1>
+  
+  <h1 class="title">Qu'est ce que TraLELHo</h1>
+  
+  Dans les hôpitaux, l’accueil de personnes étrangères n’est pas toujours simple. Comment demander en effet à un patient ne parlant pas le français d’expliquer ses douleurs ou ses antécédents médicaux ? Un casse-tête linguistique auquel tous les personnels médicaux ont un jour été confrontés. 
+  Tralelho est un site qui permet de traduire les phrases utilisées par les services de secours ou de soins dans un maximum de langues. A ce jour 101 langues sont traduites ou en cours de traductions.
+ 
+      <h1 class="title">Soutenir TraLELHo</h1>
+    
+Soutenir TraLELHo c'est d'abord l'utiliser, le diffuser auprès de vos collègues, amis.
+C'est aussi nous contacter si vous parlez une langue étrangère pour nous proposer une correction ou une nouvelle traduction.
+
+>>>>>Lien vers l'email de contact <<<<<<
+    
+Enfin soutenir TraLELHo peut aussi passer par un don, afin d'aider le site à rester héberger aussi longtemps que possible, ou a obtenir de nouvelles traductions ou corrections.
+Avec 5 euros vous permettez au site d'être hébergé un mois de plus.
+Avec 20 euros vous permettez de traduire les phrases nécessaires aux urgences dans une nouvelle langue.
+Avec 50 euros vous permettez de traduire les phrases utiles au SAMU dans une nouvelle langue.
+Avec 859 euros vous permettez de traduire une nouvelle langue (et ses 859 phrases)
+
+   <h1 class="title">On parle de Tralelho</h1>
+   
+   https://www.leparisien.fr/societe/sante/les-questions-medicales-traduites-dans-toutes-les-langues-avec-tralelho-02-11-2018-7933467.php
+   
+   https://le-quotidien-du-patient.fr/article/tous-pour-un/2019/01/21/tralelho/
+   
+   https://france3-regions.francetvinfo.fr/bretagne/ille-et-vilaine/rennes/rennes-infirmiere-urgences-concoit-tralelho-briser-barriere-langue-1606739.html
+   
+   https://www.20minutes.fr/sante/2271679-20180516-rennes-chu-langues-etrangeres-plus-secrets-personnel-medical
+   
+   https://www.ouest-france.fr/bretagne/rennes-35000/video-grace-marion-sait-parler-138-langues-aux-urgences-du-chu-de-rennes-6179604
+   
+   https://www.francebleu.fr/infos/societe/rennes-tralelho-permets-de-traduire-50-questions-medicales-en-195-langues-1523178137
+   
+   https://www.actusoins.com/308702/tralelho-un-traducteur-universel-pour-briser-la-barriere-de-la-langue-dans-les-hopitaux.html
+   
+   https://solidarites-sante.gouv.fr/systeme-de-sante-et-medico-social/parcours-de-sante-vos-droits/bonnes-pratiques-en-region/bretagne/article/tralelho
+   
+   https://www.europe1.fr/emissions/la-france-bouge-avec-raphaelle-duchemin/raphaelle-duchemin-les-innovations-dans-le-domaine-de-la-traduction-3894444
+   
+   https://www.tvr.bzh/v/57d2235-tvr-events-13-10-2020
+   
+   https://www.actusoins.com/349854/marion-verdaguer-infirmiere-chevalier-dans-lordre-du-merite.html
+ 
+ 
   </div>
 </template>
