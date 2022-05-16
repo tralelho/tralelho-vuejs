5a1fd833173f50695811dbbb66bb419be4061f5e
Tralelho-Marion <84783201+tralelho-github@users.noreply.github.com>
2022-05-16 10:05:06 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: #a2c0de;
+  background-color: #70a2d4;
 }
 @import "src/assets/main";
 </style>
