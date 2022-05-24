a44a1a000c0e2d2fef031fbf2fd5f2cae9f942c4
Tralelho-Marion <84783201+tralelho-github@users.noreply.github.com>
2022-05-24 17:27:34 +0200

    scroll sur les ancres

--- src/views/Translation.vue
+++ src/views/Translation.vue
@@ -124,6 +124,10 @@ let selectedLanguage = ref(
   max-width: 500px;
 }
 
+.box columns m-2 py-2 is-clickable change-background-on-hover {
+  scroll-margin-top: (-60px;
+  }
+
 .box columns m-2 py-2 is-clickable change-background-on-hover:hover {
   background-color: #fac3f2;
 }
