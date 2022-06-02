d94a4b1a4cf547ea3694daa6900820a81a16348e
Tralelho-Marion <84783201+tralelho-github@users.noreply.github.com>
2022-06-02 17:42:08 +0200

    marges

--- src/views/Translation.vue
+++ src/views/Translation.vue
@@ -109,14 +109,14 @@ let selectedLanguage = ref(
   flex-wrap: wrap;
   align-content: center; /* used this for multiple child */
   align-items: center; /* if an only child */
-  margin-top: -10%;
+  margin-top:0%;
 }
 
 .menu-element-multiline {
   width: 25%;
   position: fixed;
   font-size: 18px;
-  margin-top: -10%;
+  margin-top: 0%;
 }
 
 .menu-list {
