967004ce2ef39d83eac96ae68abb997f8731553d
Tralelho-Marion <84783201+tralelho-github@users.noreply.github.com>
2022-06-04 15:29:40 +0200

    marges

--- src/views/Samu.vue
+++ src/views/Samu.vue
@@ -150,7 +150,7 @@ import pageConfig from "./Samu.config.json";
   position: fixed;
   left: 25px;
   background-color: #87a7c7;
-  top:-50px;
+  top:-30px;
   max-height: 79vh;
   overflow: auto;
 }
