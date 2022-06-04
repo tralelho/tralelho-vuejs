951e5c2818ad01d4c48a4aaac7b74322039fdd30
Tralelho-Marion <84783201+tralelho-github@users.noreply.github.com>
2022-06-04 15:25:26 +0200

    correction bug

--- src/views/Samu.vue
+++ src/views/Samu.vue
@@ -150,7 +150,7 @@ import pageConfig from "./Samu.config.json";
   position: fixed;
   left: 25px;
   background-color: #87a7c7;
-  top:-50px
+  top:-50px;
   max-height: 79vh;
   overflow: auto;
 }
